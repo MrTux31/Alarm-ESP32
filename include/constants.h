97c1d12304a37892491a64ab9b8596128dd3e7bf
MrTux31 <89023193+MrTux31@users.noreply.github.com>
2026-09-06 14:29:51 +0200

    Update loop names to English translations

--- include/constants.h
+++ include/constants.h
@@ -1,11 +1,13 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+// These items are examples! Adjust them to your own needs.
+
 //Loops
-const char* const NAME_LOOP_1 = "Entrée";
-const char* const NAME_LOOP_2 = "Rez-de-chaussé";
-const char* const NAME_LOOP_3 = "Étage";
-const char* const NAME_LOOP_4 = "Compteur électrique";
+const char* const NAME_LOOP_1 = "Entrance";
+const char* const NAME_LOOP_2 = "Window living room";
+const char* const NAME_LOOP_3 = "Window bedroom";
+const char* const NAME_LOOP_4 = "Garage door";
 
 const int PIN_LOOP_1 = 13; 
 const int PIN_LOOP_2 = 12;
@@ -24,7 +26,7 @@ const unsigned long ARMING_DELAY = 25000; //Exit delay
 const unsigned long ENTRY_DELAY = 12000; //Delay for the main door (to have the time to disarm)
 const unsigned long DEFAULT_SIREN_DURATION_MS = 200000; 
 
-
+//Arm / Disarm Button
 const int PIN_BTN_ARM  = 27; 
 
 
